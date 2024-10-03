a0c33b17a0916201e244ab3325215f88d738e244
Grégory Houllier <gregory.houllier@gmail.com>
2024-10-03 08:26:55 +0000

    chore: add fix lint script

--- scripts/prerelease.ts
+++ scripts/prerelease.ts
@@ -1,30 +1,30 @@
 import { EOL } from "node:os";
 
 interface Manifest {
-    name: string;
-    version: string;
-    license: string;
+  name: string;
+  version: string;
+  license: string;
 }
 
-interface JsrManifest extends Manifest{
-    exports: string;
+interface JsrManifest extends Manifest {
+  exports: string;
 }
 
 const jsr = Bun.file("./jsr.json", { type: "application/json" });
-const jsrContent: Pick<JsrManifest, 'license'> = await jsr.json();
+const jsrContent: Pick<JsrManifest, "license"> = await jsr.json();
 const pkg = Bun.file("./package.json", { type: "application/json" });
 const pkgContent: Manifest = await pkg.json();
 
 await Bun.write(
-    jsr,
-    `${JSON.stringify(
-        {
-            ...jsrContent,
-            name: pkgContent.name,
-            version: pkgContent.version,
-            license: pkgContent.license,
-        },
-        null,
-        2
-    )}${EOL}`
+  jsr,
+  `${JSON.stringify(
+    {
+      ...jsrContent,
+      name: pkgContent.name,
+      version: pkgContent.version,
+      license: pkgContent.license,
+    },
+    null,
+    2,
+  )}${EOL}`,
 );
